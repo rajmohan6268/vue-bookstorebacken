e1af9acbd9aab2db6efebdbbd206e17b275fb3b0
rajmohan6268 <rajmohan6268@gmail.com>
2021-11-09 10:34:49 +0530

    fix

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-function initial() {
+async function initial() {
   Role.estimatedDocumentCount((err, count) => {
     if (!err && count === 0) {
       new Role({
